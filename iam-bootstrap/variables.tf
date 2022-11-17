7095fe7609ef28d8c3f0667d03dc0262e0403619
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2022-11-16 17:24:56 -0800

    IAM bootstrap module

---
+++ iam-bootstrap/variables.tf
@@ -0,0 +1,32 @@
+variable "deploy_id" {
+  type        = string
+  description = "Domino Deployment ID"
+
+  validation {
+    condition     = can(regex("^[a-z-0-9]{3,32}$", var.deploy_id))
+    error_message = "Argument deploy_id must: start with a letter, contain lowercase alphanumeric characters(can contain hyphens[-]) with length between 3 and 32 characters."
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region for the deployment"
+}
+
+variable "iam_policy_paths" {
+  type        = list(any)
+  description = "IAM policies to provision and use for deployment role, can be terraform templates"
+  default     = []
+}
+
+variable "template_config" {
+  type        = map
+  description = "Variables to use for any templating in the IAM policies. AWS account ID (as 'account_id'), deploy_id and region are automatically included."
+  default     = {}
+}
+
+variable "max_session_duration" {
+  type        = number
+  description = "Maximum session duration for role in seconds"
+  default     = 43200
+}
