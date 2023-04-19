d3134106f778261daa741d3dc9335748e57589d1
Luis De Bello <luis.debello@dominodatalab.com>
2023-04-19 14:12:41 -0300

    Fixing formatting

--- submodules/eks/k8s.tf
+++ submodules/eks/k8s.tf
@@ -1,13 +1,13 @@
 module "k8s_setup" {
   count = var.bastion_info != null || var.eks.public_access.enabled ? 1 : 0
 
-  source        = "../k8s"
+  source          = "../k8s"
   template_prefix = "k8s_setup"
-  ssh_key       = var.ssh_key
-  bastion_info  = var.bastion_info
-  network_info  = var.network_info
-  eks_info      = local.eks_info
-  cluster_setup = true
+  ssh_key         = var.ssh_key
+  bastion_info    = var.bastion_info
+  network_info    = var.network_info
+  eks_info        = local.eks_info
+  cluster_setup   = true
 
   depends_on = [aws_eks_addon.vpc_cni, null_resource.kubeconfig]
 }
@@ -15,13 +15,13 @@ module "k8s_setup" {
 module "calico_setup" {
   count = var.bastion_info != null || var.eks.public_access.enabled ? 1 : 0
 
-  source         = "../k8s"
+  source          = "../k8s"
   template_prefix = "calico_setup"
-  ssh_key        = var.ssh_key
-  bastion_info   = var.bastion_info
-  network_info   = var.network_info
-  eks_info       = local.eks_info
-  install_calico = true
+  ssh_key         = var.ssh_key
+  bastion_info    = var.bastion_info
+  network_info    = var.network_info
+  eks_info        = local.eks_info
+  install_calico  = true
 
   depends_on = [aws_eks_node_group.node_groups, module.k8s_setup]
 }
