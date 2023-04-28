8d4cc32798a2042947bcff0c309fd78ae49a0145
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2023-04-27 17:51:56 -0700

    Fix bad reference to kms key

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "aws_default_tags" "this" {}
 
 locals {
-  kms_key = var.kms.enabled ? (var.kms.key_id != null ? data.aws_kms_key.key[0].id : aws_kms_key.domino[0]) : null
+  kms_key = var.kms.enabled ? (var.kms.key_id != null ? data.aws_kms_key.key[0] : aws_kms_key.domino[0]) : null
   kms_info = local.kms_key != null ? {
     key_id  = local.kms_key.id
     key_arn = local.kms_key.arn
